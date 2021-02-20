68156e4a431eb44f135537b28a3a58503bd88754
Sara Wilson <sarakatewilson98@gmail.com>
2021-02-20 13:07:31 -0500

    adding engine to go through handlebars

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Requiring necessary npm packages
 const express = require("express");
+const exphbs=require("express-handlebars");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
@@ -11,12 +12,17 @@ const db = require("./models");
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 app.use(express.urlencoded({ extended: true }));
+
+app.engine('handlebars',exphbs());
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
 
