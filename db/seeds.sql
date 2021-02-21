dbebe63831c12fc318daee62628c6a11c68dd9df
Olivia Park <oliviaypark113@gmail.com>
2021-02-21 18:46:17 -0500

    updated set up shop.handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO closets (item, color ,size, price, createdAt, updatedAt)
+ VALUES ('Glynis cute Tee', 'yellow','M', '29.99',now(),now());
+ INSERT INTO closets (item, color ,size, price, createdAt, updatedAt)
+ VALUES ('Sarah stylish Sweatshirts', 'grey','M', '39.99',now(),now());
+  INSERT INTO closets (item, color ,size, price, createdAt, updatedAt)
+ VALUES ('Olivia skinny Legging', 'grey','M', '39.99',now(),now());
+  INSERT INTO closets (item, color ,size, price, createdAt, updatedAt)
+ VALUES ('Nick chic Sweatpant', 'grey','M', '39.99',now(),now());
